7efea2236890ffae77075db6144d17b8603baa33
Ольга Старикова <lelik.starickova@yandex.ru>
2025-12-04 12:18:57 +0300

    пробел

--- src/ru/yandex/practicum/WordNotFoundException.java
+++ src/ru/yandex/practicum/WordNotFoundException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum;
 
-public class WordNotFoundException extends Exception{
+public class WordNotFoundException extends Exception {
     public WordNotFoundException(String message) {
         super(message);
     }
--- src/ru/yandex/practicum/Wordle.java
+++ src/ru/yandex/practicum/Wordle.java
@@ -103,5 +103,4 @@ public class Wordle {
         }
 
     }
-
 }
